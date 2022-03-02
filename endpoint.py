33b20d08ec9f0991d14b2c2ca5e39690b7b42f88
Mykhailo Buialo <mk@buialo.com>
2022-03-02 16:31:41 +0200

    added endpoint for reciving link

--- endpoint.py
+++ endpoint.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from diia import diia
 
 app = FastAPI()
 
 
-@app.post("/diia-reciver")
+@app.post("/diia-receiver")
 async def diia_reciver(data):
     print(data)
     return { "success": True }
+
+class link_generator(BaseModel):
+    user_id: str
+    return_link: str
+
+@app.post("/link-generator")
+async def link_generator(data:link_generator):
+    deeplink = diia.create_deeplink(
+        branch_id=diia.branch_id,
+        return_deeplink=data.return_link,
+        user_id=data.user_id)
+    return { "deeplink": deeplink }
